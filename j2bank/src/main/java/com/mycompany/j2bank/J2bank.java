a21a6ad2af92ebb0e20fa772439598f5cfaa979f
bcmenezess <darkdragonbr48@gmail.com>
2025-02-15 15:45:51 -0300

    iniciando repositorio j2bank

---
+++ j2bank/src/main/java/com/mycompany/j2bank/J2bank.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.j2bank;
+
+/**
+ *
+ * @author B r u n o
+ */
+public class J2bank {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

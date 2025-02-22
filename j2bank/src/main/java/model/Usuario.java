b1702046a87909cc808f34afae2990d6d5cab2dc
bcmenezess <darkdragonbr48@gmail.com>
2025-02-22 20:18:21 -0300

    model simples para testes

---
+++ j2bank/src/main/java/model/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author B r u n o
+ */
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String tipo;
+    
+    
+    public Usuario(String nome, String senha, String tipo){
+        this.nome = nome;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+}

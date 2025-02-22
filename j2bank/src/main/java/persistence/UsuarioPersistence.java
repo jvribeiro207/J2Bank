de0291078c4f5d70fdf80ad8159947a02a0a3f27
bcmenezess <darkdragonbr48@gmail.com>
2025-02-22 20:20:58 -0300

    usuarioPersistence para salvar e procurar registros

---
+++ j2bank/src/main/java/persistence/UsuarioPersistence.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package persistence;
+
+import persistence.*;
+import model.Usuario;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+import com.google.gson.Gson;
+
+/**
+ *
+ * @author B r u n o
+ */
+public class UsuarioPersistence implements Persistence<Usuario> {
+
+    private static final String PATH = DIRECTORY + File.separator + "usuarios.json";
+
+    @Override
+    public void save(List<Usuario> itens) {
+        Gson gson = new Gson();
+        String json = gson.toJson(itens);
+
+        File diretorio = new File(DIRECTORY);
+        if (!diretorio.exists()) {
+            diretorio.mkdirs();
+        }
+
+        Arquivo.salva(PATH, json);
+
+    }
+
+    @Override
+    public List<Usuario> findAll() {
+        Gson gson = new Gson();
+
+        String json = Arquivo.le(PATH);
+
+        List<Usuario> usuarios = new ArrayList<>();
+        if (!json.trim().equals("")) {
+
+            Type tipoLista = new TypeToken<List<Usuario>>() {
+            }.getType();
+            usuarios = gson.fromJson(json, tipoLista);
+
+            if (usuarios == null) {
+                usuarios = new ArrayList<>();
+            }
+        }
+
+        return usuarios;
+    }
+}

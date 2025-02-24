c937a8a39a747d730acd2bb336bfc32b3056452d
bcmenezess <darkdragonbr48@gmail.com>
2025-02-23 21:32:41 -0300

    cria model de renda variavel

---
+++ j2bank/src/main/java/model/RendaVar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author B r u n o
+ */
+public class RendaVar extends Opcoes {
+    
+    public RendaVar(String nome_op, String rendimento) {
+        super(nome_op, rendimento);
+    }
+    
+}

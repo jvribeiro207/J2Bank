de99f8f0225619d3c5792f64df74cf04a366d24f
bcmenezess <darkdragonbr48@gmail.com>
2025-02-24 02:59:44 -0300

    altera model de renda variavel, seus atributos e metodos

--- j2bank/src/main/java/model/RendaVar.java
+++ j2bank/src/main/java/model/RendaVar.java
@@ -10,8 +10,40 @@ package model;
  */
 public class RendaVar extends Opcoes {
     
-    public RendaVar(String nome_op, String rendimento) {
+    private String tipo_op;
+    private int percentual_risco;
+
+    public RendaVar(String tipo_op, int percentual_risco, String nome_op, String rendimento) {
         super(nome_op, rendimento);
+        this.tipo_op = tipo_op;
+        this.percentual_risco = percentual_risco;
+    }
+
+    public String getTipo_op() {
+        return tipo_op;
+    }
+
+    public void setTipo_op(String tipo_op) {
+        this.tipo_op = tipo_op;
+    }
+
+    public int getPercentual_risco() {
+        return percentual_risco;
+    }
+
+    public void setPercentual_risco(int percentual_risco) {
+        this.percentual_risco = percentual_risco;
+    }
+    
+    @Override
+    public String toString() {
+        return getNome_op() + 
+                " - " +
+                getTipo_op() +
+                " - " +
+                "Risco: " + getPercentual_risco() + "%" +
+                " - " +
+                "Rendimento esperado: " + getRendimento();
     }
     
 }

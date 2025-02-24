cbaecdfb4b86e02b104c95ce9dfb6797f904ae80
bcmenezess <darkdragonbr48@gmail.com>
2025-02-23 21:31:36 -0300

    cria model de cliente

---
+++ j2bank/src/main/java/model/Cliente.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author B r u n o
+ */
+public class Cliente extends Usuario {
+    
+    public Cliente(String nome, String senha, String tipo) {
+        super(nome, senha, tipo);
+    }
+    
+}

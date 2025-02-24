ea126e10930d428c1825b8b1cc5d45cdda9019be
jvribeiro207 <>
2025-02-24 17:03:29 -0300

    faz alterações na class cliente

--- j2bank/src/main/java/model/Cliente.java
+++ j2bank/src/main/java/model/Cliente.java
@@ -3,12 +3,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model;
-
+import java.math.BigDecimal;
+import java.util.List;
 /**
  *
  * @author B r u n o
  */
 public class Cliente extends Usuario {
+    
+    private BigDecimal saldo;
+    private List<Transacao> historicoTransacoes;
     
     public Cliente(String nome, String senha, String tipo) {
         super(nome, senha, tipo);

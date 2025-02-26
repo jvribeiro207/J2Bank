44b2050681b40c3e1c0feb525908140713c05183
jose <jose@pop-os>
2025-02-26 14:42:04 -0300

    Cria o arquivo Controller do Caixa

---
+++ j2bank/src/main/java/controller/CaixaController.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.DefaultListModel;
+import model.Caixa;
+import persistence.Persistence;
+import persistence.CaixaPersistence;
+
+
+/**
+ *
+ * @author jose
+ */
+public class CaixaController {
+    private DefaultListModel<Caixa> model;
+
+    public CaixaController(DefaultListModel<Caixa> model) {
+        this.model = model;
+    }
+    
+    public void carregaCaixas() {
+        Persistence<Caixa> caixaPersistence = new CaixaPersistence();
+        List<Caixa> todos = caixaPersistence.findAll();
+        
+        for (Caixa caixa : todos) {
+            model.addElement(caixa);
+        }
+    }
+    
+    public List<Caixa> listaCaixas() {
+        List<Caixa> caixas = new ArrayList<>();
+        for (int i = 0; i < model.size(); i++) {
+            caixas.add(model.get(i));
+        }
+        return caixas;
+    }
+    
+    public void salvaCaixasAoFechar() {
+        Persistence<Caixa> caixaPersistence = new CaixaPersistence();
+        caixaPersistence.save(listaCaixas());
+    }
+    
+    public void removeCaixa(int indice) {
+        model.remove(indice);
+    }
+}

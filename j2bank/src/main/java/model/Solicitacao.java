e40c4f7e9876c80c8fd6cc8bc4645457e761409d
joaov <joaov@DESKTOP-UQ3ISH7>
2025-03-03 17:50:14 -0300

    cria model de solicitacao

---
+++ j2bank/src/main/java/model/Solicitacao.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author joaov
+ */
+public class Solicitacao {
+    
+    private String cpfCliente;
+    private BigDecimal valor;
+    private String tipoSolicitacao;
+    private String status;
+    private String data;
+
+    public Solicitacao(String cpfCliente, BigDecimal valor, String tipoSolicitacao,String status, String data) {
+        this.cpfCliente = cpfCliente;
+        this.valor = valor;
+        this.tipoSolicitacao = tipoSolicitacao;
+        this.status = status;
+        this.data = data;
+    }
+
+    public String getCpfCliente() {
+        return cpfCliente;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public String getTipoSolicitacao() {
+        return tipoSolicitacao;
+    }
+    public String getStatus(){
+        return status;
+    }
+    
+    public void setStatus(String status){
+        this.status = status;
+    }
+    
+    public BigDecimal getValor() {
+        return valor;
+    }
+ 
+}
